bc803c194e9f459243cd03931f87c10999baa0fa
LiHechao <lihechao@kingsoft.com>
2022-09-01 14:37:27 +0800

    第五章完成，使用了基本的数据库

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,24 +1,62 @@
+import time
+import os
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import unittest
 import pytest
+
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlists.settings")
+django.setup()
+from lists.models import Item
 
 class NewVistorTest(unittest.TestCase):
     def setUp(self):
         service = Service('d://chromedriver')
         option = webdriver.ChromeOptions()
-        self.driver = webdriver.Chrome(service=service, options=option)
-        self.browser = self.driver
+        self.browser = webdriver.Chrome(service=service, options=option)
+        #self. = self.driver
         self.url = "http://127.0.0.1:8000/"
 
     def tearDown(self):
-        self.driver.quit()
+        self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.driver.get(self.url)
+        self.browser.get(self.url)
 
         assert "To-Do" in self.browser.title
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        #header_text = self.browser.find_element_by_tag_name('h1').text
+        assert 'To-Do' in header_text
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        assert inputbox.get_attribute('placeholder') == 'Enter a to-do item'
+
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        assert any(['Buy peacock feathers' in row.text for row in rows])
+
+
+        # second
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        assert any(['Buy peacock feathers' in row.text for row in rows])
+        assert any(['Use peacock feathers to make a fly' in row.text for row in rows])
+        time.sleep(3)
         #pytest.fail('finish the test')
+
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
-
+from lists.models import Item
 
 # Create your tests here.
 
@@ -16,9 +17,69 @@ class HomePageTest(TestCase):
         found = resolve("/")
         self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
+    def test_0001_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b"<html>"))
-        self.assertIn(b'<title>To-Do Lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b"</html>"))
+        content = response.content.decode("utf-8-sig").encode('utf-8')
+        print(f'{type(content) =}, {content}')
+
+        self.assertTrue(content.startswith(b"<html>"))
+        self.assertIn(b'<title>To-Do Lists</title>', content)
+        self.assertTrue(content.endswith(b"</html>"))
+        # failed for csrf
+        # self.assertEqual(response.content.decode(), render_to_string('home.html'))
+
+
+    def test_0002_home_page_can_save_a_post_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        response = home_page(request)
+
+        self.assertEqual(Item.objects.count(), 1)
+
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_0003_home_page_redirect_after_post(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        response = home_page(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+        return
+
+
+
+        content = response.content.decode("utf-8-sig").encode('utf-8').decode()
+        self.assertTrue('A new list item' in content)
+        expected_html = render_to_string('home.html', {'new_item_text': 'A new list item'})
+        #print(f'{type(content.decode()) = }')
+        #print(f'{content.decode() = }')
+        self.assertEqual(content[:100], expected_html[:100])
+
+
+    def test_0004_home_page_displays_all_list_item(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
+
+class ItemModelTest(TestCase):
+    def test_0001_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first list item'
+        first_item.save()
+
+        item = Item()
+        item.text = 'The second list item'
+        item.save()
+
+        items = Item.objects.all()
+        assert items.count() == 2
+
+
